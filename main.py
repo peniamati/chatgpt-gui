c8da9b202ea4f24761384f171f2ddecdab67b270
peniamati <pena_matias@hotmail.com>
2023-04-11 20:33:26 -0300

    Implement send message and get response from bot

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QLineEdit, QPushButton
 import sys
+from backend import Chatbot
+import threading
 
 
 class ChatbotWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.chatbot = Chatbot()
 
         self.setMinimumSize(700, 500)
 
@@ -16,12 +20,26 @@ class ChatbotWindow(QMainWindow):
         # Add input field widget
         self.input_field = QLineEdit(self)
         self.input_field.setGeometry(10, 340, 480, 40)
+        self.input_field.returnPressed.connect(self.send_message())
 
         # Add the button
         self.button = QPushButton("Send", self)
         self.button.setGeometry(500, 340, 100, 40)
+        self.button.clicked.connect(self.send_message)
 
         self.show()
+
+    def send_message(self):
+        user_input = self.input_field.text().strip()
+        self.chat_area.append(f"<p style='color:#333333'>Me: {user_input}</p>")
+        self.input_field.clear()
+
+        thread = threading.Thread(target=self.get_bot_response, args=(user_input, ))
+        thread.start()
+
+    def get_bot_response(self, user_input):
+        response = self.chatbot.get_response(user_input)
+        self.chat_area.append(f"<p style='color:#333333; background-color:#E9E9E9'> Bot: {response}</p>")
 
 
 app = QApplication(sys.argv)
