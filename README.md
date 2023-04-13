b823937886ee5807afca01b58b2349ffe72075cd
peniamati <pena_matias@hotmail.com>
2023-04-12 22:29:53 -0300

    Final touches, add readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# What does this app?
+Its an PyQt6 GUI app connected to open ai api
--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class ChatbotWindow(QMainWindow):
         # Add input field widget
         self.input_field = QLineEdit(self)
         self.input_field.setGeometry(10, 340, 480, 40)
-        self.input_field.returnPressed.connect(self.send_message())
+        self.input_field.returnPressed.connect(self.send_message)
 
         # Add the button
         self.button = QPushButton("Send", self)
